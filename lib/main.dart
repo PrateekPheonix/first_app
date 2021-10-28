661f49a1318771fdfb6319a0868f47b41a3cdff0
Prateek Niket <prateekniket25@gmail.com>
2021-10-28 20:13:44 +0530

    added some buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -6,12 +8,32 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What is your favorite colour?",
+      "What is your favorite animal",
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My first app"),
         ),
-        body: Text("Default Text"),
+        body: Column(
+          children: [
+            Text("The Questions"),
+            RaisedButton(
+              child: Text("Answer 1"),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text("Answer 2"),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text("Answer 3"),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
